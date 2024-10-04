2525f7b44f952a37ffba7648fc70a76868965414
Anthony Campos <anthonyrubencampos@gmail.com>
2024-10-04 19:49:35 -0400

    Initial Commit

---
+++ Stupedia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stupedia
+//
+//  Created by Anthony Campos on 10/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stupedia/StupediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StupediaApp.swift
+//  Stupedia
+//
+//  Created by Anthony Campos on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct StupediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
